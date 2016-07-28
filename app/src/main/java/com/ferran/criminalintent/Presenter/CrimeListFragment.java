85d402f4233a2d8922031416bd07947d8a1134b0
Ferran <jjzzhlhl@hotmail.com>
2016-07-28 22:17:34 +0800

    ViewPager

---
+++ app/src/main/java/com/ferran/criminalintent/Presenter/CrimeListFragment.java
@@ -0,0 +1,139 @@
+package com.ferran.criminalintent.Presenter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ferran.criminalintent.Model.Crime;
+import com.ferran.criminalintent.Model.CrimeLab;
+import com.ferran.criminalintent.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrimeListFragment extends Fragment {
+    private static final int REQUEST_CRIME = 1;
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+    private int changePostion;
+//    private CharSequence changeTitle;
+//    private boolean changeSolved;
+//    private boolean needChange = false;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (resultCode != Activity.RESULT_OK) {
+//            return;
+//        }
+//        if (requestCode == REQUEST_CRIME) {
+//            if (data == null) {
+//                return;
+//            }
+//            needChange = true;
+//            changeTitle = CrimeFragment.changedTitle(data);
+//            changeSolved = CrimeFragment.changedSolved(data);
+//        }
+//    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+//            if (needChange == true) {
+//                crimes.get(changePostion).setTitle(changeTitle.toString());
+//                crimes.get(changePostion).setSolved(changeSolved);
+//                needChange = false;
+//            }
+
+            mAdapter.notifyItemChanged(changePostion);
+        }
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
+        private Crime mCrime;
+
+        public CrimeHolder(final View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    changePostion = getAdapterPosition();
+                    Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getID());
+                    startActivityForResult(intent, REQUEST_CRIME);
+                }
+            });
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
+
+        }
+
+        public void bindCrime(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(
+                    new SimpleDateFormat("EEEE, MMM dd, yyyy, HH:mm").format(mCrime.getDate()));
+            mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = new ArrayList<>(crimes);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.bindCrime(crime);
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
+            return new CrimeHolder(view);
+        }
+    }
+}
